4e1262c0b4fcf511b3bba28d39059b5eac3224ae
Will Sackfield <will.sackfield@gmail.com>
2025-05-06 07:14:38 -0400

    Add shift to process

--- timeseriesfeatures/process.py
+++ timeseriesfeatures/process.py
@@ -8,6 +8,7 @@ from .lag_process import lag_process
 from .non_categorical_numeric_columns import \
     find_non_categorical_numeric_columns
 from .rolling_process import rolling_process
+from .shift_process import shift_process
 
 
 def process(
@@ -15,8 +16,10 @@ def process(
     windows: list[datetime.timedelta | None] | None = None,
     lags: list[int] | None = None,
     on: str | None = None,
+    shift: int = 1,
 ) -> pd.DataFrame:
     """Process the dataframe for timeseries features."""
+    original_features = df.columns.values.tolist()
     features = find_non_categorical_numeric_columns(df)
     if lags is None:
         lags = []
@@ -24,4 +27,8 @@ def process(
         windows = []
     df = lag_process(df, lags, features)
     df = rolling_process(df, windows, on, features)
-    return df
+    added_features = [
+        x for x in df.columns.values.tolist() if x not in original_features
+    ]
+    df = shift_process(df, features + added_features, shift)
+    return df[sorted(df.columns.values.tolist())]
